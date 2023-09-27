6acee7d4b8e0c0ca06c76308a651abb678f5ce38
Nagesh Joshi <nagesh.joshi@rapidcircle.com>
2023-09-27 12:34:02 +0530

    Lookup query and column param changed to optional

--- script/Query-LookupTable.ps1
+++ script/Query-LookupTable.ps1
@@ -1,84 +1,93 @@
-param(
-    [string] $TableName,
-    [string] $Query,
-    [string] $Column,
-    [string] $TenantId,
-    [string] $ClientId,
-    [string] $ClientSecret,
-    [string] $LookupServiceIdentifier
-)
+function Get-LookupValue {
+    param(
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $TableName,
 
-#$GitHubToken
+        [Parameter(Mandatory = $false)]
+        [string] $Query,
 
-$TableName
-$Column
-$Query
-$LookupServiceIdentifier
+        [Parameter(Mandatory = $false)]
+        [string] $Column,
 
-Write-Host "Fetching lookup details from Table: $($TableName)"
-Write-Host "Query: $($Query)"
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $TenantId,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $ClientId,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $ClientSecret,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $LookupServiceIdentifier
+    )
+
+    #$GitHubToken
+
+    $TableName
+    $Column
+    $Query
+    $LookupServiceIdentifier
+
+    Write-Host "Fetching lookup details from Table: $($TableName)"
+    Write-Host "Query: $($Query)"
 
 
-#################################################################################################################
-Write-Host "Getting access token"
+    #################################################################################################################
+    Write-Host "Getting access token"
 
-$env:LookupResourceId
-try {
-    $res = Invoke-RestMethod -Method POST `
-        -Uri "https://login.microsoftonline.com/$TenantId/oauth2/token" `
-        -Body @{ resource = $LookupServiceIdentifier; grant_type = "client_credentials"; client_id = $ClientId; client_secret = $ClientSecret }`
-        -ContentType "application/x-www-form-urlencoded"
-    $access_token = $res.access_token
-    #$access_token
-    if ($access_token) {
-        Write-Host "Access token fetched successfully"
+    $env:LookupResourceId
+    try {
+        $res = Invoke-RestMethod -Method POST `
+            -Uri "https://login.microsoftonline.com/$TenantId/oauth2/token" `
+            -Body @{ resource = $LookupServiceIdentifier; grant_type = "client_credentials"; client_id = $ClientId; client_secret = $ClientSecret }`
+            -ContentType "application/x-www-form-urlencoded"
+        $access_token = $res.access_token
+        #$access_token
+        if ($access_token) {
+            Write-Host "Access token fetched successfully"
+        }
     }
-}
-catch {
-    Write-Host "An exception occurred while fetching access token for lookup $($_.Exception.Message)"
-}
-
-#################################################################################################################
-#################################################################################################################
-
-Write-Host "Getting lookup values"
-try {
-    $Uri = "$($env:HostURL)/api/Lookup/odata/$($TableName)?`$filter=$Query"
-
-    $reqHeaders = @{
-        "Authorization" = "Bearer $($access_token)";
-        "Accept"        = "application/json";
+    catch {
+        Write-Host "An exception occurred while fetching access token for lookup $($_.Exception.Message)"
     }
 
-    $lookupResponse = Invoke-RestMethod -Method GET -Uri $Uri -Headers $reqHeaders
+    #################################################################################################################
+    #################################################################################################################
 
-    $lookupResponse
+    Write-Host "Getting lookup values"
+    try {
+        $Uri = "$($env:HostURL)/api/Lookup/odata/$($TableName)?`$filter=$Query"
 
-    if ($lookupResponse) {
-        $LkupValue = $lookupResponse | ConvertTo-Json -Compress
-    }
+        $reqHeaders = @{
+            "Authorization" = "Bearer $($access_token)";
+            "Accept"        = "application/json";
+        }
+
+        $lookupResponse = Invoke-RestMethod -Method GET -Uri $Uri -Headers $reqHeaders
+
+        $lookupResponse
+
+        if ($lookupResponse) {
+            $LkupValue = $lookupResponse | ConvertTo-Json -Compress
+        }
     
-    $LkupValue
+        $LkupValue
 
-    Write-Output "LookupValue=$LkupValue" >> $env:GITHUB_OUTPUT
+        Write-Output "LookupValue=$LkupValue" >> $env:GITHUB_OUTPUT
 
-    $env:GITHUB_OUTPUT
+        $env:GITHUB_OUTPUT
+    }
+    catch {
+        Write-Host "An exception occurred while fetching access token for lookup $($_.Exception.Message)"
+    }
 }
-catch {
-    Write-Host "An exception occurred while fetching access token for lookup $($_.Exception.Message)"
-}
 
-
-# try {
-#     $Uri = "https://api.github.com/repositories/672865066/environments/Staging/secrets"
-#     $reqHeaders = @{
-#         "Authorization"        = "Bearer $($GitHubToken)";
-#         "Accept"               = "application/vnd.github+json";
-#         "X-GitHub-Api-Version" = "2022-11-28"
-#     }
-#     $res = Invoke-RestMethod -Method GET -Uri $Uri -Headers $reqHeaders
-#     $res
-# }
-# catch {
-#     $_
-# }
+Get-LookupValue -TableName $TableName -Query $Query -Column $Column `
+    -TenantId $TenantId ClientId $ClientId -ClientSecret $ClientSecret `
+    -LookupServiceIdentifier $LookupServiceIdentifier
